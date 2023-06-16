dbe7441c976b9edfec2afa75a5a442ba18506b57
jan1ooo <filhojanio67@gmail.com>
2023-06-16 20:13:51 -0300

    Project Created

---
+++ src/main/java/com/jan1ooo/systemrh/SystemRhSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.systemrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemRhSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemRhSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jan1ooo/systemrh/SystemRhSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.systemrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemRhSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
